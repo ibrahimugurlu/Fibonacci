c502e888d1a7543ff943335a162fface3eddb9fd
Ibrahim Ugurlu <ibrahimugurlu18@outlook.com>
2022-03-12 11:14:47 +0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int before, now, next;
+            before = 0;
+            now = 1;
+            for (int i = 0; i < 25; i++)
+            {
+                next = before + now;
+                before = now;
+                now = next;
+                Console.WriteLine(next);
+            }
+           
+            Console.Read();
+        }
+    }
+}
